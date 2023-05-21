5b806b4b147c6f4e127da7bfe891bced206c615f
zeus <zeuskeyz@gmail.com>
2023-05-21 16:20:10 +0300

    FINISHED PROJECT

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,5 +1,7 @@
 package org.example;
 
+import org.example.dao.HeroDao;
+import org.example.dao.SquadDao;
 import org.example.db.database;
 import org.example.model.Hero;
 import org.example.model.Squad;
@@ -7,7 +9,6 @@ import org.sql2o.Connection;
 import spark.ModelAndView;
 import spark.template.handlebars.HandlebarsTemplateEngine;
 
-import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -16,74 +17,34 @@ import static spark.Spark.*;
 
 // Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
 // then press Enter. You can now see whitespace characters in your code.
-public class Main {
+
+public abstract class Main {
+
     public static void main(String[] args) {
+
         staticFileLocation("/public");
         HandlebarsTemplateEngine views = new HandlebarsTemplateEngine();
 
-        get("/", (req,res) -> {
-            List<Hero> allHeroes = null;
-            List<Squad> allSquads = null;
-            try(Connection db = database.getConnect().open()){
-                String heroes = "SELECT * FROM heroes;";
-                String squads = "SELECT * FROM squads";
-                allHeroes = db.createQuery(heroes).executeAndFetch(Hero.class);
-                allSquads = db.createQuery(squads).executeAndFetch(Squad.class);
+        //LANDING PAGE
+        get("/", (req,res) -> new ModelAndView(new HashMap<>(),"landing-page.hbs"), views );
 
-            } catch (Exception error) { System.out.println(error.getMessage());}
+        //HOME PAGE
+        get("/home", (req,res) -> {
 
             Map<String, Object> combinedList = new HashMap<>();
-            combinedList.put("hero", allHeroes);
-            combinedList.put("squad", allSquads);
-            System.out.println(combinedList);
-
+            combinedList.put("heroObject", HeroDao.getAllHeroes());
+            combinedList.put("squadObject", SquadDao.getAllSquads());
             return new ModelAndView(combinedList, "home.hbs");
-            },views);
 
-        get("/assign-squad/:name", (req,res) -> {
-            String heroParam = "";
-            List<Squad> allSquads = null;
-            try(Connection db = database.getConnect().open()){
-                String heroes = "SELECT * FROM heroes WHERE name=(:name);";
-                String squads = "SELECT * FROM squads";
-                heroParam =  req.params(":name");
-                allSquads = db.createQuery(squads).executeAndFetch(Squad.class);
-
-            } catch (Exception error) { System.out.println(error.getMessage());}
-
-            Map<String, Object> combinedList = new HashMap<>();
-            combinedList.put("hero", heroParam);
-            combinedList.put("squad", allSquads);
-
-            return new ModelAndView(combinedList, "assign-squad.hbs");
         },views);
 
-        post("/assign-squad/:name", (req,res) -> {
-            String name = req.queryParams("name");
-            String squad = req.queryParams("squad");
-            Integer age = 0;
-            String power = "";
-            Integer power_score = 0;
-            String weakness = "";
-            Integer weakness_score = 0;
-
-
-            Hero newHero = new Hero(name,age,power,power_score,weakness,weakness_score,squad);
-
-            try(Connection db = database.getConnect().open()){
-                String heroUpdate = "UPDATE heroes SET squad = (:squad) WHERE name = (:name)";
-                db.createQuery(heroUpdate).bind(newHero).executeUpdate();
-            } catch (Exception error) { System.out.println(error.getMessage());}
-            res.redirect("/");
-            return null;
-        },views);
-
-
+        //HERO ADDING FORM PAGE
         get("/add-hero", (req,res) -> new ModelAndView(new HashMap<>(),"add-hero.hbs"), views );
 
+        //SENDING HERO DETAILS TO DATABASE
         post("/add-hero", (req,res)-> {
 
-            String name = req.queryParams("name");
+            String hero = req.queryParams("hero");
             Integer age = Integer.parseInt(req.queryParams("age"));
             String power = req.queryParams("power");
             Integer power_score = Integer.parseInt(req.queryParams("power_score"));
@@ -91,42 +52,85 @@ public class Main {
             Integer weakness_score = Integer.parseInt(req.queryParams("weakness_score"));
             String squad = "";
 
-            Hero newHero = new Hero(name.toUpperCase(),age,power,power_score,weakness,weakness_score,squad);
-
-            try(Connection db = database.getConnect().open()){
-                String heroAdd = "INSERT INTO heroes (name,age,power,power_score,weakness,weakness_score) VALUES (:name, :age, :power, :power_score, :weakness, :weakness_score);";
-                db.createQuery(heroAdd).bind(newHero).executeUpdate();
-
-            } catch (Exception error) { System.out.println(error.getMessage());}
-
-            res.redirect("/");
-
+            Hero newHero = new Hero(hero.toUpperCase(),age,power,power_score,weakness,weakness_score,squad);
+            HeroDao.addHero(newHero);
+            res.redirect("/home");
             return null ;
         });
 
-        
+        //SQUAD ADDING FORM PAGE
         get("/add-squad", (req,res) -> new ModelAndView(new HashMap<>(),"add-squad.hbs"), views );
 
+        //SENDING HERO DETAILS TO DATABASE
         post("/add-squad", (req,res)-> {
 
-            String name = req.queryParams("name");
+            String squad = req.queryParams("squad");
             String cause = req.queryParams("cause");
             Integer size = Integer.parseInt(req.queryParams("size"));
-            ArrayList<String> members = new ArrayList<String>();
-
-            Squad newSquad = new Squad(name,cause,size,members);
-
-            try(Connection db = database.getConnect().open()){
-                String squadAdd = "INSERT INTO squads (name,cause, size) VALUES (:name, :cause, :size);";
-                db.createQuery(squadAdd).bind(newSquad).executeUpdate();
-
-            } catch (Exception error) { System.out.println(error.getMessage());}
-
-            res.redirect("/");
-
+            Squad newSquad = new Squad(squad,cause,size);
+            SquadDao.addSquad(newSquad);
+            res.redirect("/home");
             return null;
 
         });
 
+        get("/assign-squad/:squad", (req,res) -> {
+
+            String squad =  req.params("squad");
+            Map<String, Object> combinedList = new HashMap<>();
+
+            if(HeroDao.heroCount(squad) < SquadDao.maxSize(squad)) {
+                combinedList.put("querySquad", squad);
+                combinedList.put("heroObject", HeroDao.membership(squad));
+            } else res.redirect("/full-squad");
+
+            return new ModelAndView(combinedList, "assign-squad.hbs");
+        },views);
+
+        //ASSIGNING A HER0 TO A SQUAD
+        post("/assign-squad/:squad", (req,res) -> {
+
+            String hero = req.queryParams("hero");
+            String squad = req.queryParams("squad");
+            HeroDao.updateMembership(hero, squad);
+            res.redirect("/home");
+            return null;
+
+        },views);
+
+        //DELETING A HERO FROM THE PAGE
+        get("/delete-hero/:hero", (req,res)-> {
+
+            String name = req.params(":hero");
+            HeroDao.deleteHero(name);
+            res.redirect("/home");
+            return null;
+
+        },views);
+
+        //DELETING A SQUAD FROM THE PAGE
+        get("/delete-squad/:squad", (req,res)-> {
+
+            String name = req.params(":squad");
+            SquadDao.deleteSquad(name);
+            res.redirect("/home");
+            return null;
+
+        },views);
+
+        //CREATES A PAGE WITH SEARCHABLE TABLE LIST OF ALL HEROES AND SQUADS
+        get("/all", (req,res) -> {
+
+            Map<String, Object> combinedList = new HashMap<>();
+            combinedList.put("heroObject", HeroDao.getAllHeroes());
+            combinedList.put("squadObject", SquadDao.getAllSquads());
+            return new ModelAndView(combinedList, "all.hbs");
+
+        },views);
+
+        //FULL SQUAD ERROR PAGE
+        get("/full-squad", (req,res) -> new ModelAndView(new HashMap<>(),"full-squad.hbs"), views );
+
     }
 }
+
--- src/main/java/org/example/model/Squad.java
+++ src/main/java/org/example/model/Squad.java
@@ -1,2 +1,50 @@
-package org.example.model;public class Squad {
+package org.example.model;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+
+public class Squad {
+    private  String squad;
+    private  String cause;
+    private  Integer size;
+
+    public String getSquad() {
+        return squad;
+    }
+
+    public void setSquad(String squad) {
+        this.squad = squad;
+    }
+
+    public String getCause() {
+        return cause;
+    }
+
+    public void setCause(String cause) {
+        this.cause = cause;
+    }
+
+    public Integer getSize() {
+        return size;
+    }
+
+    public void setSize(Integer size) {
+        this.size = size;
+    }
+
+
+    public Squad(String squad, String cause, Integer size) {
+        this.squad = squad;
+        this.cause = cause;
+        this.size = size;
+    }
+
+    @Override
+    public String toString() {
+        return "Squad{" +
+                "squad='" + squad + '\'' +
+                ", cause='" + cause + '\'' +
+                ", size=" + size +
+                '}';
+    }
 }
